93f9593774cf1b8813acb44feb139f1607dbfc65
Diego <diego.b25@hotmail.com>
2019-10-15 15:34:48 -0500

    inicio de aplicacion del mvc

---
+++ vistas/plantilla.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>RedDents</title>
+	<!-- favicon -->
+	<link rel="shorcut icon" href="vistas/img/Logo_blue.png">
+	<!-- estilos -->
+	<link rel="stylesheet" type="text/css" href="vistas/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="vistas/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="vistas/css/estilos.css">
+	<!-- scripts -->
+	<script src="vistas/js/jquery-3.4.1.min.js"></script>
+	<script src="vistas/js/popper.min.js"></script>
+	<script src="vistas/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<header>
+					<nav class="navbar navbar-expand-lg navbar-dark barnav">
+				  		<a class="navbar-brand" href="#"><img class="logo" src="img/Logotipo-white.png" alt="RedDents"></a>
+				  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+				    		<span class="navbar-toggler-icon"></span>
+				  		</button>
+
+					</nav>
+				</header>
+			</div>
+			<div class="col-lg-12 contenedor" style="">
+				
+			</div>
+	</div>
+</body>
+</html>
